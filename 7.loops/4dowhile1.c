4facb1c9a285706609a9246990f764a5e62e117a
Florin Stanciu <florinspad@gmail.com>
2021-05-19 16:37:44 +0300

    while , do-while -- update

---
+++ 7.loops/4dowhile1.c
@@ -0,0 +1,17 @@
+// Learning C
+// Florin S
+
+//do-while  loop
+
+#include <stdio.h>
+int main()
+{
+    int count = 1;
+    do
+    {
+        printf("Count=%d\n", count);
+        count++;
+    } while (count < 8);
+
+    return 0;
+}
---
+++ 7.loops/9while1.c
@@ -0,0 +1,15 @@
+// Learning C
+// Florin S
+
+// while  loop  (pre-tested condition)
+#include <stdio.h>
+int main()
+{
+    int count = 1;
+    while (count < 8)
+    {
+        printf("Count=%d\n", count);
+        count++;
+    }
+    return 0;
+}
