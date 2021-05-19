a0a1d6345fe51db227776bf888cc59bf9921059d
Florin Stanciu <florinspad@gmail.com>
2021-05-19 12:26:17 +0300

    conditional example , rearranging folders

---
+++ 11.typeCasting/1_typeCasting.c
@@ -0,0 +1,21 @@
+// Learning C
+// Florin S
+
+// type casting
+// covert one data tye into another
+// syntax : --->     (type) value
+
+// cast int value into float
+
+#include <stdio.h>
+int main()
+{
+    int n = 9 / 4;
+    printf("int : %d\n", n);
+
+    float f = (float)9 / 4;
+
+    printf("float: %f\n", f);
+
+    return 0;
+}
---
+++ 5.conditional/3_conditional.c
@@ -0,0 +1,13 @@
+// Learning C
+// Florin S
+
+// if
+
+#include <stdio.h>
+int main()
+{
+    int score = 89;
+    if (score > 75)
+        printf("You passed!\n");
+    return 0;
+}
